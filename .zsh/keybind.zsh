1302f50ced812707893bb72b5525d0d06099792c
Lycolia Rizzim <33796432+Lycolia@users.noreply.github.com>
2026-04-22 10:53:44 +0900

    最新のMSYS2でHome, Endが効かない問題を修正
    キーコードがfreebsd互換になっていたため統合した

--- .zsh/keybind.zsh
+++ .zsh/keybind.zsh
@@ -2,18 +2,8 @@
 ## Del
 bindkey '^[[3~' delete-char
 
-if [[ $OSTYPE =~ 'msys|cygwin' ]]; then
-    # for MSYS2
-    ## arrow up
-    bindkey '^[[A' up-line-or-search
-    ## arrow down
-    bindkey '^[[B' down-line-or-search
-    ## Home
-    bindkey "\e[H" beginning-of-line
-    ## End
-    bindkey "\e[F" end-of-line
-elif [[ `echo $OSTYPE | grep freebsd` ]]; then
-    # for sakura internet
+if [[ $OSTYPE =~ 'msys|cygwin|freebsd*' ]]; then
+    # for MSYS2 or sakura internet
     ## arrow up
     bindkey '^[[A' up-line-or-search
     ## arrow down
